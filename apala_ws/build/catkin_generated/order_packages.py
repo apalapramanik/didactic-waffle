0ba26cb473c96f54fb570b1bd37e0525bd46bcaf
Apala <apalapramanikraka@gmail.com>
2023-07-12 13:49:25 -0500

    updated world

--- apala_ws/build/catkin_generated/order_packages.py
+++ apala_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/apramani/didactic-waffle/apala_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/apramani/didactic-waffle/apala_ws/devel;/opt/ros/noetic'.split(';') if '/home/apramani/didactic-waffle/apala_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
