6ce2b906a32b94378dee39b9e1bfd857554935c9
Apala <apalapramanikraka@gmail.com>
2023-07-19 13:57:35 -0500

    update

--- apala_ws/src/tb_apala/scripts/yolo_depth.py
+++ apala_ws/src/tb_apala/scripts/yolo_depth.py
@@ -51,12 +51,13 @@ class ObjectDetectionNode:
     
         
     def detect_objects(self):
+        
         # Set the depth scale
         # depth_scale = 0.0010000000474974513
-        depth_scale = 0.001
+        # depth_scale = 0.001
 
         # Convert the depth image to meters
-        depth_image = self.depth_image * depth_scale
+        depth_image = self.depth_image #* depth_scale
 
         # Detect objects using YOLOv5
         results = self.model(self.color_image)
@@ -68,23 +69,20 @@ class ObjectDetectionNode:
 
         # Process the results
         for result in results.xyxy[0]:
-            x1, y1, x2, y2, confidence, class_id = result
+            x1, y1, w, h, confidence, class_id = result
             # print(type(confidence))
 
             # Calculate the center of the bounding box
-            center_x = (x1 + x2) / 2
-            center_y = (y1 + y2) / 2
+            center_x = (x1 + w) / 2
+            center_y = (y1 + h) / 2
             
 
             # Calculate the depth at the center of the bounding box
             object_depth = depth_image[int(center_y), int(center_x)]
             
             # Calculate the minimum depth within the region covered by the bounding box
-            # object_depth = np.min(depth_image[int(y1):int(y2), int(x1):int(x2)])
+            # object_depth = np.min(depth_image[int(y1):int(h), int(x1):int(w)])
 
-            # Convert the depth from the depth scale to meters  
-            object_depth = object_depth.item() * depth_scale  
-            
             
             # Publish the class ID, class name, depth and confidence
             self.yolo_depth_msg.class_id = int(class_id)
@@ -92,43 +90,43 @@ class ObjectDetectionNode:
             self.yolo_depth_msg.depth = object_depth            
             self.yolo_depth_msg.confidence = confidence
             self.yolo_info_pub.publish(self.yolo_depth_msg) 
-                     
-            object_depth = "{:.3e}m".format(object_depth) 
+                    
+            # object_depth = "{:.3e}m".format(object_depth) 
             depth = str(object_depth)      
 
             # Draw a rectangle around the object
-            cv2.rectangle(self.color_image, (int(x1), int(y1)), (int(x2), int(y2)), (153, 26, 25), 2)
+            cv2.rectangle(self.color_image, (int(x1), int(y1)), (int(w), int(h)), (153, 26, 25), 2)
 
             # Add text for depth
             cv2.putText(self.color_image, depth, (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (153, 26, 25), 1)
-            cv2.putText(self.color_image, self.model.names[int(class_id)] , (int(x1), int(y1)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (153, 26, 25), 1)
+            cv2.putText(self.color_image, self.model.names[int(class_id)] , (int(x1), int(y1)+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (153, 26, 25), 1)
             
+            class_id = int(class_id)
+        
             # Check if the detected object is a person (class_id 0 for 'person')
-            if int(class_id) == 0:
+            if class_id == 0:
                 persons.append((center_x, center_y, object_depth))
+                # print(len(persons))
             else:
                 other_objects.append((center_x, center_y, object_depth))
+                # print(len(other_objects))
 
         # If no person is detected, publish "no" and return
-        if not persons:
-            self.cloud_processing_pub.publish("no")
-            return
-
-        # Find the closest person to compare distances
-        closest_person = min(persons, key=lambda x: x[2])
-        print("closest person distance:", closest_person)
-
-        # Check distances of other objects and publish the result
-        for obj in other_objects:
-            if obj[2] < closest_person[2]:
-                print("object_distance:", obj[2], "person distance:", closest_person[2])
-                self.cloud_processing_pub.publish("no")
-            else:
-               print("object_distance:", obj[2], "person distance:", closest_person[2])
-               self.cloud_processing_pub.publish("yes") 
+        if len(persons) == 0:
+            self.cloud_processing_pub.publish("noooo")
+        else:
+            # Find the closest person to compare distances
+            closest_person = min(persons, key=lambda x: x[2])
+            print("closest person distance:", closest_person[2])
+            if len(other_objects) > 0:
+                for obj in other_objects:
+                    if obj[2] < closest_person[2]:
+                        # print("object_distance:", obj[2], "person distance:", closest_person[2])
+                        self.cloud_processing_pub.publish("no")
+                    else:
+                        # print("object_distance:", obj[2], "person distance:", closest_person[2])
+                        self.cloud_processing_pub.publish("yes") 
                 
-      
-            
 
         # Convert the annotated image back to ROS Image message
         annotated_image_msg = self.bridge.cv2_to_imgmsg(self.color_image, encoding='bgr8')
@@ -140,12 +138,7 @@ class ObjectDetectionNode:
         # Show the annotated image
         cv2.imshow("Annotated Image", self.color_image)
         cv2.waitKey(1)
-        
-        
-# Function to calculate Euclidean distance between two points
-def calculate_distance( point1, point2):
-    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-       
+ 
 
 def main():
     rospy.init_node('Depth_calculation', anonymous=False)    
