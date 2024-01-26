bd6247fbcc04c73cfa40b062a6a4a005fc1facc9
Apala <apalapramanikraka@gmail.com>
2024-01-26 14:03:48 -0600

    updates

--- teb_ws/src/verification/src/reachability_node.py
+++ teb_ws/src/verification/src/reachability_node.py
@@ -3,125 +3,124 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from tf.transformations import euler_from_quaternion
 import numpy as np
-from StarV.plant.dlode import DLODE
-from StarV.set.star import Star
 from StarV.set.probstar import ProbStar
 import math
 
 
-class robot_reachability:
     
-    def __init__():
-        pass
+def compute_reachability( bb_vertices_human,  X_initial, std_initial, A ,  std_dev=2 , steps=3, robot_width = 0.281, robot_length = 0.306 ):
+    model_dt = 0.25/10
+    # vel, omega = U_initial
+
+    # Convert Initial State to Probstar 
+    c = (np.expand_dims(X_initial, axis=0)).transpose()
+    V = np.diag(std_initial)
+    n_sigma = np.diag(np.ones(X_initial.shape[0]))
+    n_mu = np.zeros(X_initial.shape[0])
+    l = np.ones(X_initial.shape[0]) * std_dev * -1
+    u = np.ones(X_initial.shape[0]) * std_dev
     
-    def compute_reachability( initial_state, X_initial, std_initial, U_initial, A ,  std_dev=2 , steps=3 ):
-        model_dt = 0.25/10
-        vel, omega = U_initial
+    probstars = []
 
-        # Convert Initial State to Probstar 
-        c = (np.expand_dims(X_initial, axis=0)).transpose()
-        V = np.diag(std_initial)
-        n_sigma = np.diag(np.ones(X_initial.shape[0]))
-        n_mu = np.zeros(X_initial.shape[0])
-        l = np.ones(X_initial.shape[0]) * std_dev * -1
-        u = np.ones(X_initial.shape[0]) * std_dev
-      
-        probstars = []
-
-        # Iterate Model Until k = reachability_start_idx
-        for i in range(steps):
-            for j in range(10):
-                c = np.matmul(A, c)
-                V = np.matmul(A, V)
-
-       
-        for i in range(steps):
-            for j in range(10):
-                c = np.matmul(A,c)
-                V = np.matmul(A, V)
-
-            # Apply Collision Bounding Predicate
-            # H,g = collision_predicate.two_car_predicate(i,0.25,X_0[4:8],V_pi,zeta_pi,V_omega,zeta_omega)
-            # C = np.matmul(H,V)
-            # d = g-np.matmul(H,c)
-            # d = np.asarray(d).squeeze()
-            # c_V_Combine =  np.concatenate([c,V], axis=1)
-            # c_V_Combine = np.asarray(c_V_Combine)
-            # V = np.asarray(V)
-            # C = np.asarray(C)
-            # probstar = ProbStar(c_V_Combine,C,d,n_mu,n_sigma)
-            # probstars.append(probstar)
-        return probstars
-    
-    def bounding_box_vertices(width, length, angle):
-        # angle = math.atan2(y2 - y1, x2 - x1)
-        x1 = (length/2) * math.cos(angle) - (width/2) * math.sin(angle)
-        y1 = (width/2) * math.cos(angle) + (length/2) * math.sin(angle)
-        x2 = -(length/2) * math.cos(angle) - (width/2) * math.sin(angle)
-        y2 = (width/2) * math.cos(angle) - (length/2) * math.sin(angle)
-        x3 = (length/2) * math.cos(angle) + (width/2) * math.sin(angle)
-        y3 = -(width/2) * math.cos(angle) + (length/2) * math.sin(angle)
-        x4 = -(length/2) * math.cos(angle) + (width/2) * math.sin(angle)
-        y4 = -(width/2) * math.cos(angle) - (length/2) * math.sin(angle)
-        bounding_box_vertices = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-        return bounding_box_vertices
-    
-    
-    
-    def add_edges(edges, bb_vertices, box_index):
-            for i in range(len(bb_vertices)):
-                next_idx = (i + 1) % len(bb_vertices)
-                dx = bb_vertices[next_idx][0] - bb_vertices[i][0]
-                dy = bb_vertices[next_idx][1] - bb_vertices[i][1]
-                edges.append([dx, dy, box_index, i])
-
-    def minkowski_difference_2d_convex_hull(bb1, bb2):
-        
-        # Convert to minkowski addition problem by negating vertices of the second bounding box
-        bb2_negated = [[-x, -y] for x, y in bb2]
-
-        # Sort vertices by polar angle
-        bb1.sort(key=lambda vertex: math.atan2(vertex[1], vertex[0]))
-        bb2_negated.sort(key=lambda vertex: math.atan2(vertex[1], vertex[0]))
-
-        # Create edges for both bounding boxes
-        edges = []        
-
-        robot_reachability.add_edges(edges, bb1, 1)
-        robot_reachability.add_edges(edges, bb2_negated, 2)
-
-        # Sort edges by polar angle
-        edges.sort(key=lambda edge: math.atan2(edge[1], edge[0]))
-
-        # Calculate the starting point as the sum of positions of starting vertices in the first two edges
-        first_edge_bb1 = next(edge for edge in edges if edge[2] == 1)
-        first_edge_bb2 = next(edge for edge in edges if edge[2] == 2)
-        starting_x = bb1[first_edge_bb1[3]][0] + bb2[first_edge_bb2[3]][0]
-        starting_y = bb1[first_edge_bb1[3]][1] + bb2[first_edge_bb2[3]][1]
-
-        minkowski_sum_bbox = []
-        current_point = (starting_x, starting_y)
-
-        # Calculate Minkowski sum
-        for edge in edges:
-            current_point = (current_point[0] + edge[0], current_point[1] + edge[1])
-            minkowski_sum_bbox.append(current_point)
-
-        return minkowski_sum_bbox
-    
-    def convex_hull_vertex_array_to_linear_constraint(convex_hull_array):
-        num_vertices = len(convex_hull_array)
-        C = np.zeros((num_vertices, 2))
-        d = np.zeros((num_vertices, 1))
-
-        for idx in range(num_vertices):
-            x1, y1 = convex_hull_array[idx]
-            x2, y2 = convex_hull_array[(idx + 1) % num_vertices]
-
-            C[idx, 0] = -(y2 - y1)
-            C[idx, 1] = x2 - x1
-            d[idx, 0] = -(y1 * (x2 - x1) - x1 * (y2 - y1))
-
-        return C, d
+    # Iterate Model Until k = reachability_start_idx
+    for i in range(steps):
+        for j in range(10):
+            c = np.matmul(A, c)
+            V = np.matmul(A, V)
 
     
+    for i in range(steps):
+        for j in range(10):
+            c = np.matmul(A,c)
+            V = np.matmul(A, V)
+
+        # Apply Collision Bounding Predicate
+        H,g = predicates(i,bb_vertices_human, robot_width, robot_length)
+        C = np.matmul(H,V)
+        d = g-np.matmul(H,c)
+        d = np.asarray(d).squeeze()
+        c_V_Combine =  np.concatenate([c,V], axis=1)
+        c_V_Combine = np.asarray(c_V_Combine)
+        V = np.asarray(V)
+        C = np.asarray(C)
+        probstar = ProbStar(c_V_Combine,C,d,n_mu,n_sigma)
+        probstars.append(probstar)
+    return probstars
+
+def bb_vertices(width, length, angle):
+   
+    x1 = (length/2) * math.cos(angle) - (width/2) * math.sin(angle)
+    y1 = (width/2) * math.cos(angle) + (length/2) * math.sin(angle)
+    x2 = -(length/2) * math.cos(angle) - (width/2) * math.sin(angle)
+    y2 = (width/2) * math.cos(angle) - (length/2) * math.sin(angle)
+    x3 = (length/2) * math.cos(angle) + (width/2) * math.sin(angle)
+    y3 = -(width/2) * math.cos(angle) + (length/2) * math.sin(angle)
+    x4 = -(length/2) * math.cos(angle) + (width/2) * math.sin(angle)
+    y4 = -(width/2) * math.cos(angle) - (length/2) * math.sin(angle)
+    bounding_box_vertices = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+    return bounding_box_vertices
+
+def add_edges(edges, bb_vertices, box_index):
+        for i in range(len(bb_vertices)):
+            next_idx = (i + 1) % len(bb_vertices)
+            dx = bb_vertices[next_idx][0] - bb_vertices[i][0]
+            dy = bb_vertices[next_idx][1] - bb_vertices[i][1]
+            edges.append([dx, dy, box_index, i])
+
+def minkowski_difference_2d_convex_hull(bb1, bb2):
+    
+    # Convert to minkowski addition problem by negating vertices of the second bounding box
+    bb2_negated = [[-x, -y] for x, y in bb2]
+
+    # Sort vertices by polar angle
+    bb1.sort(key=lambda vertex: math.atan2(vertex[1], vertex[0]))
+    bb2_negated.sort(key=lambda vertex: math.atan2(vertex[1], vertex[0]))
+
+    # Create edges for both bounding boxes
+    edges = []        
+
+    add_edges(edges, bb1, 1)
+    add_edges(edges, bb2_negated, 2)
+
+    # Sort edges by polar angle
+    edges.sort(key=lambda edge: math.atan2(edge[1], edge[0]))
+
+    # Calculate the starting point as the sum of positions of starting vertices in the first two edges
+    first_edge_bb1 = next(edge for edge in edges if edge[2] == 1)
+    first_edge_bb2 = next(edge for edge in edges if edge[2] == 2)
+    starting_x = bb1[first_edge_bb1[3]][0] + bb2[first_edge_bb2[3]][0]
+    starting_y = bb1[first_edge_bb1[3]][1] + bb2[first_edge_bb2[3]][1]
+
+    minkowski_sum_bbox = []
+    current_point = (starting_x, starting_y)
+
+    # Calculate Minkowski sum
+    for edge in edges:
+        current_point = (current_point[0] + edge[0], current_point[1] + edge[1])
+        minkowski_sum_bbox.append(current_point)
+
+    return minkowski_sum_bbox
+
+def convex_hull_vertex_array_to_linear_constraint(convex_hull_array):
+    num_vertices = len(convex_hull_array)
+    C = np.zeros((num_vertices, 2))
+    d = np.zeros((num_vertices, 1))
+
+    for idx in range(num_vertices):
+        x1, y1 = convex_hull_array[idx]
+        x2, y2 = convex_hull_array[(idx + 1) % num_vertices]
+
+        C[idx, 0] = -(y2 - y1)
+        C[idx, 1] = x2 - x1
+        d[idx, 0] = -(y1 * (x2 - x1) - x1 * (y2 - y1))
+
+    return C, d
+
+
+def predicates(angle, bb_vertices_human, robot_width, robot_length,):  
+    bb_vertices_robot = bb_vertices(robot_width, robot_length,  angle)    
+    minkowski_difference = minkowski_difference_2d_convex_hull(bb_vertices_robot,bb_vertices_human)
+    C_s,d_s = convex_hull_vertex_array_to_linear_constraint(minkowski_difference)
+    C = np.hstack([C_s, np.zeros((C_s.shape[0],2)),-1*C_s,np.zeros((C_s.shape[0],2))])
+    d=d_s   
+    return C,d
--- teb_ws/src/verification/src/robot_state.py
+++ teb_ws/src/verification/src/robot_state.py
@@ -15,19 +15,24 @@ import numpy as np
 from scipy.linalg import expm
 import matplotlib.pyplot as plt
 from std_msgs.msg import Float32
+from sensor_msgs.msg import PointCloud2 as pc2
 from math import cos, sin
+from verification import reachability_node
+from StarV.plant.dlode import DLODE
+from StarV.set.probstar import ProbStar
 
 # from verification.src.StarV.StarV.plant.dlode import DLODE
 
 
 
 
-class robot_state:
+class robot_human_state:
     
     def __init__(self):
         rospy.init_node('robot_state', anonymous=True)
-        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
-        self.error_pub = rospy.Publisher('/state_error', Float32, queue_size=10)  
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback,queue_size=10) 
+        self.pc_human_sub = rospy.Subscriber("projected",pc2,self.human_pc_callback,queue_size=10)
+        
         self.states_history = []
         self.errors_history = []
         
@@ -35,7 +40,36 @@ class robot_state:
         self.X = np.array([0.0, 0.0, 0.0])  # [x, y, theta]
         self.last_time = rospy.Time.now()
         
+     
+
+    def human_pc_callback(self, pose_msg):
         
+        pcl_np = pointcloud2_to_numpy(pose_msg)
+        mean_value = np.mean(pcl_np, axis=0)
+        std_deviation = np.std(pcl_np, axis=0)
+        
+        self.x_mean = mean_value[0]
+        self.y_mean = mean_value[1]
+        
+        self.x_std = std_deviation[0]
+        self.y_std = std_deviation[1]
+        
+        # Define a bounding box based on mean and standard deviation
+        bounding_box_size_multiplier = 2
+        bounding_box_width = bounding_box_size_multiplier * self.x_std
+        bounding_box_height = bounding_box_size_multiplier * self.y_std
+
+        # Calculate bounding box vertices
+        x_min = self.x_mean - bounding_box_width / 2
+        x_max = self.x_mean + bounding_box_width / 2
+        y_min = self.y_mean - bounding_box_height / 2
+        y_max = self.y_mean + bounding_box_height / 2
+
+        # Bounding box vertices in the order: top-left, top-right, bottom-right, bottom-left
+        self.bb_vertices_human = [(x_min, y_max), (x_max, y_max), (x_max, y_min), (x_min, y_min)]
+        
+
+   
     def odom_callback(self, odom_msg):
         
         # Extract pose and twist information from odometry message
@@ -54,7 +88,13 @@ class robot_state:
         vel = odom_msg.twist.twist.linear 
         omega = odom_msg.twist.twist.angular
         
-        self.U = np.array([vel, omega])     
+        self.U = np.array([vel, omega])    
+        
+        dt = 0.25
+    
+        self.A = np.array([[1.0, 0.0, -vel*cos(theta)*dt],
+                           [0.0, 1.0, -vel*sin(theta)*dt],
+                           [0.0, 0.0, 1.0]])
         
         
         if len(self.states_history) <= 19:
@@ -82,120 +122,77 @@ class robot_state:
             self.std_theta = np.std(states_array[:, 2])
 
 
-            # Print the results
-            print("Mean and standard deviation for x:")
-            print(f"Mean: {self.mean_x}, Standard Deviation: {self.std_x}")
-
-            print("\nMean and standard deviation for y:")
-            print(f"Mean: {self.mean_y}, Standard Deviation: {self.std_y}")
-
-            print("\nMean and standard deviation for theta:")
-            print(f"Mean: {self.mean_theta}, Standard Deviation: {self.std_theta}")
-            
-            self.X_initial = np.array([self.mean_x, self.mean_y, self.mean_theta])
-            self.std_initial = np.array([self.std_x, self.std_y, self.std_theta])
-            
-            
-        
-        
-        # with open('states.txt', 'a') as file:
-        #     file.write(f"{self.X}\n")
-            
-        # print("current state:", self.X, "\n")
-     
-        
-       
-        # twist_cmd = Twist()
-        # twist_cmd.linear.x = 0.1  # Example linear velocity
-        # twist_cmd.angular.z = 0.0  # Example angular velocity
-        # self.twist_pub.publish(twist_cmd)
-        
-        # self.U = np.array([twist_cmd.linear.x , twist_cmd.angular.z])
-        
-      
-
-        # Calculate state-space matrices (A, B, C, D)
-    
-        self.A = np.array([[1.0, 0.0, -vel*cos(theta)],
-                           [0.0, 1.0, -vel*sin(theta)],
-                           [0.0, 0.0, 1.0]])
-
-        # self.B = np.array([[np.cos(theta), 0],
-        #                    [np.sin(theta), 0],
-        #                    [0, 1]])
-
-        # self.C = np.eye(3)  # Output all states as measurements
-
-        # self.D = np.zeros((3, 2))  # No direct dependence on control inputs
 
    
         
-        # plant = DLODE(self.A, self.B, self.C, self.D)
-        # # plant.info()
+        plant = DLODE(self.A)
+        # plant.info()
         
-        # s = ProbStar.rand(2)
-        # print(s)
+        
+        
+        
         
         """
-        
+        s = ProbStar()
+        print(s)
         X0 = ProbStar.rand(2)
         X1, Y1 = plant.stepReach(X0)
         U0 = np.array([1.0, 0.5])
         X2, Y2 = plant.stepReach(X0, U0)
         
-        # Define Distribution for Initial State Space Vector
-        X_0,sigma_0,U_0 = initial_state.initial_state(pose_history,actuation_history,pose_dt_history)
-        V_pi,zeta_pi,V_omega,zeta_omega = U_0
-
-        # Convert Initial State to Probstar 
-        c = (np.expand_dims(X_0, axis=0)).transpose()
-        V = np.diag(sigma_0)
-        n_sigma = np.diag(np.ones(X_0.shape[0]))
-        n_mu = np.zeros(X_0.shape[0])
-        l = np.ones(X_0.shape[0]) * standard_deviations * -1
-        u = np.ones(X_0.shape[0]) * standard_deviations
-        probstars = []
-        
-        """
-        
+        """    
         
                 
-  
-        
-        # self.next_state = np.dot(self.A, self.X) + np.dot(self.B, self.U)
-        
-        # with open('next_states.txt', 'a') as file:
-        #     file.write(f"{self.next_state}\n")
-            
-        
-        # print("next state: ", self.next_state,"\n")
-        
-        # if len(self.states_history) >1:
-        #     self.error = self.next_state - self.states_history[-2]
-        # else:
-        #     self.error = None
-            
-        # Append current state and error to history lists
-        # self.errors_history.append(self.error)
-        
-        # Print and plot
-        # print("Error:", self.error)
-       
-            
-        
-        # Plotting
-        # self.plot_results()
-        
         print("---------------------------------------------------")
         
         
 if __name__ == '__main__':
    
-    robot_state_calc = robot_state()
+    robot_state_calc = robot_human_state()
     rospy.spin()
         
         
 """
+
+ # if len(self.states_history) <= 19:
+        #     self.states_history.append(self.X)
+        # else:
+        #     self.states_history.pop(0)  # Remove the oldest entry
+        #     self.states_history.append(self.X)
+
+        # print("length:", len(self.states_history))
+
+        # if len(self.states_history) == 20:
+        #     # Convert states_history to a NumPy array for easier calculations
+        #     states_array = np.array(self.states_history)
+
+        #     # Calculate mean and standard deviation for x
+        #     self.mean_x = np.mean(states_array[:, 0])
+        #     self.std_x = np.std(states_array[:, 0])
+
+        #     # Calculate mean and standard deviation for y
+        #     self.mean_y = np.mean(states_array[:, 1])
+        #     self.std_y = np.std(states_array[:, 1])
+
+        #     # Calculate mean and standard deviation for theta
+        #     self.mean_theta = np.mean(states_array[:, 2])
+        #     self.std_theta = np.std(states_array[:, 2])
+
+
+        #     # Print the results
+        #     print("Mean and standard deviation for x:")
+        #     print(f"Mean: {self.mean_x}, Standard Deviation: {self.std_x}")
+
+        #     print("\nMean and standard deviation for y:")
+        #     print(f"Mean: {self.mean_y}, Standard Deviation: {self.std_y}")
+
+        #     print("\nMean and standard deviation for theta:")
+        #     print(f"Mean: {self.mean_theta}, Standard Deviation: {self.std_theta}")
+            
+        #     self.X_initial = np.array([self.mean_x, self.mean_y, self.mean_theta])
+        #     self.std_initial = np.array([self.std_x, self.std_y, self.std_theta])
+Robot:
+
 Mean and standard deviation for x:
 Mean: 11.45685789395808, Standard Deviation: 5.868065846169667e-07
 
@@ -208,5 +205,17 @@ Mean: 1.6257967346611615, Standard Deviation: 1.031531294396443e-05
 initial state: [11.45686 -7.21802  1.62578] 
 
 """
-        
+def pointcloud2_to_numpy(pointcloud_msg):
+    # Convert the PointCloud2 message to a NumPy array
+    numpy_array = np.frombuffer(pointcloud_msg.data, dtype=np.float32).reshape(-1, pointcloud_msg.point_step // 4)
+
+    # Extract x, y, and z coordinates
+    x = numpy_array[:, 0]
+    y = numpy_array[:, 1]
+    z = numpy_array[:, 2]
+
+    # Create a NumPy array with x, y, z coordinates
+    points = np.column_stack((x, y, z))
+
+    return points      
         
